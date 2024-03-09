c7a40a5d9f51e5ba9d23e2f152b30cc6d68f09e8
Grafaffel <96660820+Grafaffel@users.noreply.github.com>
2024-03-09 18:34:03 +0100

    Add Open Graph meta tags and improve code styling

--- main.js
+++ main.js
@@ -9,12 +9,12 @@ const codeblock = document.querySelector('code');
 const toast = document.querySelector('.toast');
 
 const urlParams = new URLSearchParams(window.location.search);
-let text = urlParams.get('text') || 'Add ?text=you-text to copy some text!';
+let text = urlParams.get('text');
 text = text.replace("%20", ' ');
 text = text.replace("%2", '/');
 text = text.replace("\n", `
 `);
-codeblock.innerText = text;
+codeblock.innerText = text || 'Add ?text=you-text to copy some text!';
 
 copy(text);
 codeblock.addEventListener('click', () => copy(text));
