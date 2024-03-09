edc89eb8cdea59a1a26fdf8322bbe10defe6f380
Grafaffel <96660820+Grafaffel@users.noreply.github.com>
2024-03-09 17:56:02 +0100

    Finished Project

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 const copy = (text) => navigator.clipboard.writeText(text);
+const codeblock = document.querySelector('code');
+const toast = document.querySelector('.toast');
+
+const urlParams = new URLSearchParams(window.location.search);
+let text = urlParams.get('text') || 'Add ?text=you-text to copy some text!';
+text = text.replace("%20", ' ');
+text = text.replace("%2", '/');
+text = text.replace("\n", `
+`);
+codeblock.innerText = text;
+
+copy(text);
+
+toast.classList.add('show');
+setTimeout(() => toast.classList.remove('show'), 3000);
