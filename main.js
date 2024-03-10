19ded58f541a48834fad5fd39df51e1d6a8467a1
Grafaffel <96660820+Grafaffel@users.noreply.github.com>
2024-03-10 15:45:00 +0100

    Removed Text from URL functionality

--- main.js
+++ main.js
@@ -9,22 +9,8 @@ const codeblock = document.querySelector('code');
 const toast = document.querySelector('.toast');
 
 const urlParams = new URLSearchParams(window.location.search);
-const URLtoText = urlParams.get('url');
 
-if (URLtoText) {
-    const url = fetch(URLtoText, {
-        method: 'GET',
-        headers: {
-            'Referer': 'https://www.google.com/',
-            'Content-Type': 'text/plain',
-            'Access-Control-Allow-Origin': '*',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-        },
-
-    }).then(res => res.text());
-}
-
-let text = urlParams.get('text') || url;
+let text = urlParams.get('text');
 
 text = text.replace("%20", ' ');
 text = text.replace("%2", '/');
