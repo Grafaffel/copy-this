e318fb0494ea510843362ec2833c7208a7a17691
Grafaffel <96660820+Grafaffel@users.noreply.github.com>
2024-03-10 13:53:42 +0100

    Fixed CSS highliting styles and added get text from URL

--- main.js
+++ main.js
@@ -9,7 +9,19 @@ const codeblock = document.querySelector('code');
 const toast = document.querySelector('.toast');
 
 const urlParams = new URLSearchParams(window.location.search);
-let text = urlParams.get('text');
+const URLtoText = urlParams.get('url');
+
+const url = fetch(URLtoText, {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'text/plain',
+        'Access-Control-Allow-Origin': '*',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    },
+
+}).then(res => res.text());
+
+let text = urlParams.get('text') || url;
 
 text = text.replace("%20", ' ');
 text = text.replace("%2", '/');
