93da0c091a9f3b434bfe26f59598528c49b0175e
Grafaffel <96660820+Grafaffel@users.noreply.github.com>
2024-03-16 13:21:01 +0100

    Update meta tags and Added text editor (EXPERIMENTAL)

--- main.js
+++ main.js
@@ -5,16 +5,28 @@ const copy = (text) => {
     setTimeout(() => toast.classList.remove('show'), 3000);
 };
 
-const codeblock = document.querySelector('code');
+const codeblock = document.getElementById('code-preview');
 const toast = document.querySelector('.toast');
+const createModal = document.getElementById('create-dialog');
+const create = document.getElementById('save');
+const codeInput = document.getElementById('code-input');
 
 const urlParams = new URLSearchParams(window.location.search);
 
-let text = urlParams.get('text');
+const dialog = document.querySelector("dialog");
+dialog.addEventListener("click", (e) => {
+    if (e.target === dialog) {
+        // close the dialog
+        dialog.close();
+    }
+});
+
+let text = urlParams.get('text')
+let url
 
 text = text.replace("%20", ' ');
 text = text.replace("%2", '/');
-text = text.replace("\n", `
+text = text.replace("\\n", `
 `);
 
 let lang = urlParams.get('lang');
@@ -29,3 +41,36 @@ if (lang) {
 
 copy(text);
 codeblock.addEventListener('click', () => copy(text));
+codeblock.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    createModal.showModal()
+    codeInput.value = text;
+    codeInput.focus();
+});
+codeInput.addEventListener('input', (e) => {
+    url = e.target.value;
+    url = url.replace(' ', '%20');
+    url = url.replace('+', '%2B');
+    url = url.replace('/', '%2F');
+    url = url.replace('\\n', '%0A');
+    url = url.replace('\n', '%0A');
+    url = url.replace('\r', '%0D');
+    url = url.replace('\t', '%09');
+    url = url.replace('#', '%23');
+    url = url.replace('&', '%26');
+    url = url.replace('=', '%3D');
+    url = url.replace('?', '%3F');
+    url = url.replace('@', '%40');
+
+    text = e.target.value;
+    lang = urlParams.get('lang');
+    if (lang) {
+        codeblock.innerHTML = hljs.highlight(
+            text,
+            { language: lang }
+        ).value
+    } else {
+        codeblock.innerText = text || 'Add ?text=your-text to copy some text!';
+    }
+    console.log(url);
+});
